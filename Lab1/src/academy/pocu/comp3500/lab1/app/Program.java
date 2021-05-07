56bb3fae186064bfe270d6e19fe715515a682814
QXP\mhchoi <mhchoi@qxpress.net>
2021-05-07 11:43:51 +0900

    no message

--- Lab1/src/academy/pocu/comp3500/lab1/app/Program.java
+++ Lab1/src/academy/pocu/comp3500/lab1/app/Program.java
@@ -1,8 +1,10 @@
 package academy.pocu.comp3500.lab1.app;
 
+import academy.pocu.comp3500.lab1.Lab1;
+
 public class Program {
 
     public static void main(String[] args) {
-        // Write your code here
+        System.out.println ("Lab1.add (4,5) = " + Lab1.add (4,5));
     }
 }
