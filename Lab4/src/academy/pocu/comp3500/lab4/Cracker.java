bec8f103b12e2022d94719382e9ce867f79d0de7
DESKTOP-A6L1K1R\ssamz <ssamzag@gmail.com>
2021-05-31 00:20:11 +0900

    no message

--- Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
+++ Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
@@ -26,23 +26,50 @@ public class Cracker {
 
     public String[] run(RainbowTable[] rainbowTables) {
         String[] result = new String[users.length];
-
         User me = Arrays.stream(users).filter((p) -> p.getEmail().equals(email)).collect(Collectors.toList()).get(0);
-
         int hashIndex = getHashIndex(me);
+
+        if (hashIndex == -1) {
+            return result;
+        }
 
         for (int i = 0; i < users.length; i++) {
             String passwordHash = users[i].getPasswordHash();
 
-            if (hashIndex == -1) {
-                continue;
+            if (passwordHash.length() == 24) {
+                result[i] = rainbowTables[hashIndex].get(passwordHash);
+            } else if (passwordHash.length() == 28) {
+                result[i] = rainbowTables[3].get(passwordHash);
+            } else if (passwordHash.length() == 44) {
+                result[i] = rainbowTables[4].get(passwordHash);
+            } else {
+                result[i] = rainbowTables[hashIndex].get(passwordHash);
             }
-
-            result[i] = rainbowTables[hashIndex].get(passwordHash);
         }
 
         return result;
     }
+//
+//    public String[] run(RainbowTable[] rainbowTables) {
+//
+//        String[] result = new String[users.length];
+//
+//        User me = Arrays.stream(users).filter((p) -> p.getEmail().equals(email)).collect(Collectors.toList()).get(0);
+//
+//        int hashIndex = getHashIndex(me);
+//
+//        for (int i = 0; i < users.length; i++) {
+//            String passwordHash = users[i].getPasswordHash();
+//
+//            if (hashIndex == -1) {
+//                continue;
+//            }
+//
+//            result[i] = rainbowTables[hashIndex].get(passwordHash);
+//        }
+//
+//        return result;
+//    }
 
 
     private int getHashIndex(User myInfo) {
