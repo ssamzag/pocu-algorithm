64a8907a7e5b73c8ba27ab1db5eb085f8a812f5c
DESKTOP-A6L1K1R\ssamz <ssamzag@gmail.com>
2021-05-31 00:30:20 +0900

    no message

--- Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
+++ Lab4/src/academy/pocu/comp3500/lab4/Cracker.java
@@ -3,7 +3,6 @@ package academy.pocu.comp3500.lab4;
 import academy.pocu.comp3500.lab4.pocuhacker.RainbowTable;
 import academy.pocu.comp3500.lab4.pocuhacker.User;
 
-import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -24,10 +23,11 @@ public class Cracker {
         this.password = password;
     }
 
+
     public String[] run(RainbowTable[] rainbowTables) {
         String[] result = new String[users.length];
-        User me = Arrays.stream(users).filter((p) -> p.getEmail().equals(email)).collect(Collectors.toList()).get(0);
-        int hashIndex = getHashIndex(me);
+
+        int hashIndex = getHashIndex();
 
         if (hashIndex == -1) {
             return result;
@@ -36,83 +36,54 @@ public class Cracker {
         for (int i = 0; i < users.length; i++) {
             String passwordHash = users[i].getPasswordHash();
 
-            if (passwordHash.length() == 24) {
-                result[i] = rainbowTables[hashIndex].get(passwordHash);
-            } else if (passwordHash.length() == 28) {
-                result[i] = rainbowTables[3].get(passwordHash);
-            } else if (passwordHash.length() == 44) {
-                result[i] = rainbowTables[4].get(passwordHash);
-            } else {
-                result[i] = rainbowTables[hashIndex].get(passwordHash);
-            }
+            result[i] = rainbowTables[hashIndex].get(passwordHash);
         }
 
         return result;
     }
-//
-//    public String[] run(RainbowTable[] rainbowTables) {
-//
-//        String[] result = new String[users.length];
-//
-//        User me = Arrays.stream(users).filter((p) -> p.getEmail().equals(email)).collect(Collectors.toList()).get(0);
-//
-//        int hashIndex = getHashIndex(me);
-//
-//        for (int i = 0; i < users.length; i++) {
-//            String passwordHash = users[i].getPasswordHash();
-//
-//            if (hashIndex == -1) {
-//                continue;
-//            }
-//
-//            result[i] = rainbowTables[hashIndex].get(passwordHash);
-//        }
-//
-//        return result;
-//    }
 
 
-    private int getHashIndex(User myInfo) {
-        String passwordHash = myInfo.getPasswordHash();
+    private int getHashIndex() {
+        User myInfo = Arrays.stream(users).filter((p) -> p.getEmail().equals(email)).collect(Collectors.toList()).get(0);
+        String myPasswordHash = myInfo.getPasswordHash();
 
-        if (getCrc32().equals(passwordHash)) {
+        if (getMyCrc32().equals(myPasswordHash)) {
             return 0;
         }
 
-        if (encryptThisString("MD2").equals(passwordHash)) {
+        if (encryptMyPassword("MD2").equals(myPasswordHash)) {
             return 1;
         }
 
-        if (encryptThisString("MD5").equals(passwordHash)) {
+        if (encryptMyPassword("MD5").equals(myPasswordHash)) {
             return 2;
         }
 
-        if (encryptThisString("SHA1").equals(passwordHash)) {
+        if (encryptMyPassword("SHA1").equals(myPasswordHash)) {
             return 3;
         }
 
-        if (encryptThisString("SHA256").equals(passwordHash)) {
+        if (encryptMyPassword("SHA256").equals(myPasswordHash)) {
             return 4;
         }
 
         return -1;
     }
 
-    private String encryptThisString(String hashType) {
+    private String encryptMyPassword(String hashType) {
         try {
             MessageDigest md = MessageDigest.getInstance(hashType);
 
             byte[] messageDigest = md.digest(password.getBytes(StandardCharsets.UTF_8));
 
-            Base64.Encoder encoder = Base64.getEncoder();
+            return new String(Base64.getEncoder().encode(messageDigest));
 
-            return new String(encoder.encode(messageDigest));
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private String getCrc32() {
+    private String getMyCrc32() {
         CRC32 crc32 = new CRC32();
         byte[] crc32Bytes = password.getBytes(StandardCharsets.UTF_8);
 
